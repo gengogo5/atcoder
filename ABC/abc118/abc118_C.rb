eec19722efe389b98c0b88e9a20930bcb82fec0e
gengogo5 <gen0908@gmail.com>
2019-08-21 01:00:47 +0900

    ABC118C追加

--- ABC/abc118/abc118_C.rb
+++ ABC/abc118/abc118_C.rb
@@ -1,21 +1,3 @@
 N = gets.to_i
-HPs = gets.split.map(&:to_i)
-
-#シード作り
-random=Random.new(1)
-
-#死ぬまでループ
-loop do
-  attacker=random.rand(N)
-  diffender=0
-  loop do
-    diffender=random.rand(N)
-    unless attacker == diffender
-      break
-    end
-  end
-
-
-  break
-
-end
+A = gets.split.map(&:to_i)
+puts A.inject { |a,b| a.gcd(b) }
