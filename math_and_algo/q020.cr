f4d5236a5152580483cefa58e024c12803ce6ca0
Gengo Tanaka <gen0908@gmail.com>
2022-01-10 00:34:27 +0900

    演習問題20の別解

---
+++ math_and_algo/q020.cr
@@ -0,0 +1,18 @@
+# TLEになる
+N = read_line.to_i
+A = read_line.split.map(&.to_i)
+
+cnt = 0
+(0...N).each do |i|
+  ((i+1)...N).each do |j|
+    ((j+1)...N).each do |k|
+      ((k+1)...N).each do |l|
+        ((l+1)...N).each do |m|
+          cnt += 1 if A[i] + A[j] + A[k] + A[l] + A[m] == 1000
+        end
+      end
+    end
+  end
+end
+
+puts cnt
