ddf3cbbe22de1247852883321ab89d2f61c23b2d
Gengo Tanaka <gen0908@gmail.com>
2022-02-05 23:41:11 +0900

    清書

--- ABC/abc238/ABC238_C.rb
+++ ABC/abc238/ABC238_C.rb
@@ -1,11 +1,12 @@
 N = gets.to_i
 
+# 桁数
 sl = N.to_s.length
 
 sum = 0
-(sl-1).times do |i|
-  sum += (9*(10**i) + 1) * (9*(10**i)) / 2
+sl.times do |i|
+  k = [9*(10**i),(N-(10 ** i)+1)].min
+  sum += (k % 998244353) * (k+1) / 2
 end
-sum += ((N-(10 ** (sl-1))+2) * (N-(10 ** (sl-1))+1)) / 2
 
-p sum % 998244353
+p sum
