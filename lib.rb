c06415469a084eaa1e5d0646a06928c7dd535c22
Gengo Tanaka <gen0908@gmail.com>
2022-02-06 14:27:24 +0900

    2分探索の再発明を実装

--- lib.rb
+++ lib.rb
@@ -11,3 +11,19 @@ def gismt(n);              n.times.map { gets.split.map(&:to_i) }.transpose end
 def array(n,ini=nil);      Array.new(n) { ini } end
 def darray(n1,n2,ini=nil); Array.new(n1) { Array.new(n2) { ini } } end
 def pr?(num);              Prime.prime?(num) end
+
+# tgt以上の値が最初に見つかるインデックスを返す
+# Array#bsearch_indexを車輪の再発明
+def lower_bound(array, key)
+  ng = -1 # 常に条件を満たさない(0が条件を満たす場合があるので-1)
+  ok = array.size # 常に条件を満たす
+  while (ok - ng > 1) # OK/NG領域が定まったら終了
+    mid = (ok + ng) / 2
+    if array[mid] >= key
+      ok = mid
+    else
+      ng = mid
+    end
+  end
+  ok
+end
